d66b6c4ad74f31163d67c726126726a1f0b3691a
Yuriy <yberdnikov@me.com>
2015-03-28 16:03:30 +0200

    Initial Commit

---
+++ Estimote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Estimote
+//
+//  Created by Yuriy Berdnikov on 3/28/15.
+//  Copyright (c) 2015 Yuriy Berdnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
